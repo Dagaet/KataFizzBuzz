23f03134cdbdcbf853a2ad0e83d0c196e7ecf5d4
Dagaet <alojera15@gmail.com>
2023-06-28 18:29:51 +0100

    First Part done

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    for x in 1..100 {
+        if x % 3 == 0 {
+            println!("Fizz")
+        } else if x%5 ==0{
+            println!("Buzz")
+        } else {
+            println!("{}", x);
+        };
+    }
+
+}
