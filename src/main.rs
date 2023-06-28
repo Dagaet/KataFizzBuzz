a42a29948c6d8f2c091bb5807500e30bb052df2c
Dagaet <alojera15@gmail.com>
2023-06-28 19:01:59 +0100

    Second Part done

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ fn main() {
     for x in 1..100 {
         if x % 3 == 0 {
             println!("Fizz")
-        } else if x%5 ==0{
+        } else if x.to_string().contains("3") && x.to_string().contains("5") {
+            if x % 5 == 0 {
+                println!("FizzBuzzBuzz")
+            } else {
+                println!("FizzBuzz")
+            }
+        } else if x % 5 == 0 {
             println!("Buzz")
         } else {
             println!("{}", x);
         };
     }
-
 }
